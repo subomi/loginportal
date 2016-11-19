0232b0c4745e012d4f05ca8eb9907046ba9b9a82
Subomi <subomioluwalana71@gmail.com>
2016-11-19 23:20:07 +0100

    bug fix on line 11 @ index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// Modules
+var fs = require('fs');
+
+// resources
+var users = require("./users.js");
+var Authorization = require("./lib/authorize.js");
+
+// cache file;
+var portalBanner = fs.readFileSync("node_modules/loginportal/assets/index.html", {encoding: "utf-8"});
+var Authorize = Authorization(users);
+
+// constants
+const header = "X-Beta-Authorization";
+
+// Exported Function
+module.exports = portal;
+			
+function portal(args) {
+	
+	// design & args for future usage;	
+
+	return function portal(request, response, next) {
+		var username = request.body.betaUsername;
+		var password = request.body.betaPassword;
+
+		if(!request.cookies[header]) {
+		
+			if(!(username || password)) {
+				// Respond with portalBanner
+				response.send(portalBanner);
+				return;
+			}
+			if(Authorize.authorizeUser(username, password)) {
+				// User authorized - set header & grant access - next();
+				var hashObject = Authorize.generate(username, password);
+				response.cookie(header,hashObject.hash) // {"expires": hashObject.expires});
+				response.end("Welcome continue to site");
+				return next();
+			} else {
+				// Username | password failed
+				response.end("Wrong username or password");
+			}
+		}
+		
+		if(!Authorize.test(request)) {
+			// Probably request failed to be removed from header 
+			// remove header and respond with banner
+			response.clearCookie(header);
+			response.end(portalBanner);
+			return;
+		}
+
+		// Test Passed - refresh timer
+		Authorize.refreshTimer(request.cookies[header]);
+		response.end("Welcome continue to site");
+		return next();	
+	}
+}
+
+/*
+	TODO: Write Tests
+*/
